807a00e9cc97029f03918a3cf00afc6be25edb93
Luc Castera <luc.castera@gmail.com>
2014-10-20 08:21:38 -0400

    Added Apple Stock example for linear regression

--- README.md
+++ README.md
@@ -20,7 +20,13 @@ lr.train(function(err) {
 });
 ```
 
-### Example
+### Examples
 
 [Click here](https://plot.ly/~luccastera/2) to see an example of Simple Linear Regression
-to evaluate the price of cars based on their horsepower that was done with the shaman library.
+to evaluate the price of cars based on their horsepower that was done with the shaman
+library (code is in [examples/cars.js](examples/cars.js)).
+
+[Click here](https://plot.ly/~luccastera/3/aapl-stock-prices/) to see an
+example of Simple Linear Regression applies to the stock price of AAPL
+from 2008 to 20012. Code can be seen at
+[examples/stock.js](examples/stock.js).
---
+++ examples/stock.js
@@ -0,0 +1,83 @@
+var csvParse = require('csv-parse'),
+    fs = require('fs'),
+    LinearRegression = require('../index').LinearRegression,
+    _ = require('underscore');
+
+var apiKey = process.env.PLOTLY_API_KEY;
+var username = process.env.PLOTLY_USERNAME;
+var plotly = require('plotly')(username,apiKey);
+
+fs.readFile('./examples/stock.tsv', 'utf8', function(err, dataStr) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  csvParse(dataStr, {delimiter: '\t', auto_parse: true}, function(err, data) {
+    // We are only going two columns:
+    //     x: Date
+    //     y: AAPL Stock Price
+    var x = data.map(function(h) {
+      return Date.parse(h[0]);
+    });
+    var y = data.map(function(h) { return h[1]; }); // y is price
+
+    // for the plot, we need the x values in a format that plotly can understand
+    var xForPlot = _.map(data, function(d) {
+      var date = new Date(Date.parse(d[0]));
+      return date.toISOString().slice(0,10);
+    });
+
+
+    // Initialize and train the linear regression
+    var lr = new LinearRegression(x, y);
+    lr.train(function(err) {
+      if (err) {
+        console.log('error', err);
+        process.exit(2);
+      }
+
+      // Use the linear regression function to get a set of data to graph the linear regression line
+      var y2 = [];
+      x.forEach(function(xi) {
+        y2.push(lr.predict(xi));
+      });
+
+      // Create scatter plots of training data + linear regression function
+      var layout = {
+        title: 'AAPL Stock Prices',
+        xaxis: {
+          title: 'Date'
+        },
+        yaxis: {
+          title: 'Price in $'
+        }
+      };
+      var trace1 = {
+        x: xForPlot,
+        y: y,
+        name: 'Training Data',
+        mode: "lines",
+        type: "scatter"
+      };
+      var trace2 = {
+        x: xForPlot,
+        y: y2,
+        name: 'Linear Regression',
+        mode: "lines",
+        type: "scatter"
+      };
+      var plotData = [trace1, trace2];
+      var graphOptions = {layout: layout,filename: "aapl-stock-linear-regression-with-shaman", fileopt: "overwrite"}
+      plotly.plot(plotData, graphOptions, function (err, msg) {
+        if (err) {
+          console.log(err);
+          process.exit(3);
+        } else {
+          console.log('Success! The plot (' + msg.filename + ') can be found at ' + msg.url);
+          process.exit();
+        }
+      });
+    });
+  });
+});
+
